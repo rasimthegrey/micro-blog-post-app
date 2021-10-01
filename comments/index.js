8f993af770f2aed2869b34671108df7b3c9ea00f
rasimthegrey <rasim.mutlu@protonmail.com>
2021-10-01 10:20:40 +0300

    make post request to event service

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -13,7 +14,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostID[req.params.id] || [])
 })
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentID = randomBytes(4).toString('hex')
     const { content } = req.body
 
@@ -22,7 +23,22 @@ app.post('/posts/:id/comments', (req, res) => {
     comments.push({id: commentID, content})
     commentsByPostID[req.params.id] = comments
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentID,
+            content,
+            postID: req.params.id
+        }
+    })
+
     res.status(201).send(comments)
+})
+
+app.post('/events', (req, res) => {
+    console.log('received event', req.body.type)
+
+    res.send({})
 })
 
 app.listen(4001, () => {
